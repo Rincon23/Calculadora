90bc0204db938de37739c1383ac8e17f7f76bcb6
Enzo Rincon <enzorincon2003@gmail.com>
2025-11-07 19:05:18 -0300

    Refactor calculator options from match to if-elif

--- ExerciciosCV/arquivospy/ProjetoCalculadora.py
+++ ExerciciosCV/arquivospy/ProjetoCalculadora.py
@@ -12,68 +12,67 @@ while True:
   9 - Sair""")
 
   opc = int(input("Escolha sua opção: "))
-  match opc:
 
-    case 1:
-        print("""
-        Você escolheu as operações básicas
-        1 - Soma
-        2 - Subtração
-        3 - Multiplicação
-        4 - Divisão
-        """)
-        op = int(input("Escolha sua operação: "))
-        n1 = float(input("Digite o primeiro número: "))
-        n2 = float(input("Digite o segundo número: "))
-        print()
-        match op:
-          case 1:
-              print("O resultado foi:", n1 + n2)
-          case 2:
-              print("O resultado foi:", n1 - n2)
-          case 3:
-              print("O resultado foi:", n1 * n2)
-          case 4:
-            try:
+  if opc == 1:
+      print("""
+      Você escolheu as operações básicas
+      1 - Soma
+      2 - Subtração
+      3 - Multiplicação
+      4 - Divisão
+      """)
+      op = int(input("Escolha sua operação: "))
+      n1 = float(input("Digite o primeiro número: "))
+      n2 = float(input("Digite o segundo número: "))
+      print()
+
+      if op == 1:
+          print("O resultado foi:", n1 + n2)
+      elif op == 2:
+          print("O resultado foi:", n1 - n2)
+      elif op == 3:
+          print("O resultado foi:", n1 * n2)
+      elif op == 4:
+          try:
               print("O resultado foi:", n1 / n2)
-            except ZeroDivisionError:
+          except ZeroDivisionError:
               print("Erro: divisão por zero!")
-          case _:
-                  print("Valor inválido")
-        print()
+      else:
+          print("Valor inválido")
+      print()
 
-    case 2:
-        print("Você escolheu a porcentagem")
-        val = float(input("Digite um valor: "))
-        por = float(input("Digite a porcentagem: "))
-        print("O resultado foi:", (val * por) / 100)
+  elif opc == 2:
+      print("Você escolheu a porcentagem")
+      val = float(input("Digite um valor: "))
+      por = float(input("Digite a porcentagem: "))
+      print("O resultado foi:", (val * por) / 100)
 
-    case 3:
-        print("Você escolheu a raiz quadrada")
-        val = float(input("Digite um valor: "))
-        if val < 0:
-            print("Erro: não existe raiz quadrada real de número negativo.")
-        else:
-            print("O resultado foi:", math.sqrt(val))
+  elif opc == 3:
+      print("Você escolheu a raiz quadrada")
+      val = float(input("Digite um valor: "))
+      if val < 0:
+          print("Erro: não existe raiz quadrada real de número negativo.")
+      else:
+          print("O resultado foi:", math.sqrt(val))
 
-    case 4:
-        print("Você escolheu o seno")
-        val = float(input("Digite um ângulo em graus: "))
-        print("O resultado foi:", math.sin(math.radians(val)))
+  elif opc == 4:
+      print("Você escolheu o seno")
+      val = float(input("Digite um ângulo em graus: "))
+      print("O resultado foi:", math.sin(math.radians(val)))
 
-    case 5:
-        print("Você escolheu o cosseno")
-        val = float(input("Digite um ângulo em graus: "))
-        print("O resultado foi:", math.cos(math.radians(val)))
+  elif opc == 5:
+      print("Você escolheu o cosseno")
+      val = float(input("Digite um ângulo em graus: "))
+      print("O resultado foi:", math.cos(math.radians(val)))
 
-    case 6:
-        print("Você escolheu a tangente")
-        val = float(input("Digite um ângulo em graus: "))
-        print("O resultado foi:", math.tan(math.radians(val)))
+  elif opc == 6:
+      print("Você escolheu a tangente")
+      val = float(input("Digite um ângulo em graus: "))
+      print("O resultado foi:", math.tan(math.radians(val)))
 
-    case 9:
-        print("Você saiu da calculadora.")
-        break
+  elif opc == 9:
+      print("Você saiu da calculadora.")
+      break
 
-    case _:
-        print("Valor inválido")
+  else:
+      print("Valor inválido")
